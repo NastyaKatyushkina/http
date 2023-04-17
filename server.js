efd488531e2e7ff2888edfee8cfcab32606c8d86
NastyaKatyushkina <nastyusha_kat@mail.ru>
2023-04-17 15:14:51 +0500

    http

--- server.js
+++ server.js
@@ -1,121 +1,119 @@
-const http = require('http');
-const Koa = require('koa');
-const koaBody = require('koa-body');
-const app = new Koa();
+const http = require("http")
+const fs = require("fs")
+const path = require("path")
+const Koa = require("koa")
+const koaBody = require("koa-body")
+const koaStatic = require("koa-static")
+const cors = require("@koa/cors")
 
-function getTime() {
-  const date = new Date();
-  return (`${String(date.getDate() + 1)
-    .padStart(2, '0')}.${String(date.getMonth() + 1)
-    .padStart(2, '0')}.${date.getFullYear()} ${String(date.getHours())
-    .padStart(2, '0')}:${String(date.getMinutes())
-    .padStart(2, '0')}
-  `)
-}
+const tickets = [
+	{
+		id: "1",
+		title: "Поменять красуку в принтере",
+		created: "10.03.19 08:40",
+		status: false
+	},
+	{
+		id: "2",
+		title: "Поменять красуку в принтере",
+		created: "10.03.19 08:40",
+		status: false
+	}
+]
 
-class Ticket {
-  constructor(id, name, description, status, created) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.status = status;
-    this.created = created;
-  }
-}
+const ticketsFull = [
+	{
+		id: "1",
+		title: "Поменять красуку в принтере",
+		created: "10.03.19 08:40",
+		status: false,
+		description:
+			"Ваше приложение должно реализовывать следующий функционал: Отображение всех тикетов Создание нового тикета Удаление тикета Изменение тикета Получение подробного описание тикета Отметка о выполнении каждого тикета"
+	},
+	{
+		id: "2",
+		title: "Поменять красуку в принтере",
+		created: "10.03.19 08:40",
+		status: false,
+		description:
+			"Ваше приложение должно реализовывать следующий функционал: Отображение всех тикетов Создание нового тикета Удаление тикета Изменение тикета Получение подробного описание тикета Отметка о выполнении каждого тикета"
+	}
+]
 
-const tickets = [];
+const app = new Koa()
 
-tickets.push(new Ticket(1, 'Поменять краску в принтере, ком. 404', 'Принтер HP LJ 1210, картридж на складе', false, '10.03.2019 08:40'));
-tickets.push(new Ticket(2, 'Переустановить Windows, ПК-Hall24', '', false, '15.03.2019 12:35'));
-tickets.push(new Ticket(3, 'Установить обновление КВ-ХХХ', 'Вышло критическое обновление для Windows, нужно поставить обновления в следующем приоритете: 1. Сервера (не забыть сделать бэкап!) 2. Рабочие станции', true, '15.03.2019 12:40'));
+app.use(cors())
 
+app.use(
+	koaBody({
+		urlencoded: true,
+		multipart: true
+	})
+)
 
-let currentId = tickets.length;
+app.use(ctx => {
+	const { method } = ctx.request.query
 
-app.use(koaBody({
-  urlencoded: true,
-  multipart: true,
-}));
+	switch (method) {
+		case "allTickets":
+			ctx.response.body = JSON.stringify(tickets)
+			return
+		case "createTicket":
+			const { id, title, description, status, created } = ctx.request.body
 
-app.use(async (ctx, next) => {
-  const origin = ctx.request.get('Origin');
-  if (!origin) {
-    return await next();
-  }
-  const headers = { 'Access-Control-Allow-Origin': '*', };
-  
-  if (ctx.request.method !== 'OPTIONS') {
-    ctx.response.set({...headers});
-    try {
-      return await next();
-    } catch (e) {
-      e.headers = {...e.headers, ...headers};
-      throw e;
-    }
-  }
-  
-  if (ctx.request.get('Access-Control-Request-Method')) {
-    ctx.response.set({
-      ...headers,
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
-    });
-  
-    if (ctx.request.get('Access-Control-Request-Headers')) {
-      ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Request-Headers'));
-    }
-    ctx.response.status = 204;
-  }
-});
+			tickets.push({ id, title, status, created })
+			ticketsFull.push({ id, title, description, status, created })
 
-app.use(async ctx => {
-  ctx.response.body ='server response';
+			ctx.response.body = JSON.stringify("create OK")
+			return
+		case "ticketById":
+			ctx.response.body = JSON.stringify(ticketsFull.find(({ id }) => ctx.request.query.id === id).description)
 
-  const method = ctx.request.query.method;
-  const ID = ctx.request.query.id;
-  const { id, name, description, status } = ctx.request.body;
+			return
+		case "deleteTicket":
+			const idDeleteIndex = tickets.findIndex(({ id }) => ctx.request.query.id === id)
+			tickets.splice(idDeleteIndex, 1)
+			ticketsFull.splice(idDeleteIndex, 1)
+			ctx.response.body = JSON.stringify(idDeleteIndex)
+			return
+		case "editTicket":
+			const idEditIndex = tickets.findIndex(({ id }) => ctx.request.query.id === id)
+			const requstBody = ctx.request.body
+			const ticketForEdit = tickets[idEditIndex]
+			const ticketForEditFull = ticketsFull[idEditIndex]
 
-  switch (method) {
-    case 'allTickets':
-      ctx.response.body = tickets.map(({ id, name, status, created }) => ({
-        id, name, status, created
-      }));
-      return;
-    case 'ticketById':
-      const ticketFull = tickets.find(item => item.id === +ID);
-      ctx.response.body = ticketFull;
-      return;
-    case 'createTicket':
-      currentId += 1;
-      const ticket = new Ticket(currentId, name, description, false, getTime())
-      tickets.push(ticket);
+			ticketForEdit.title = requstBody.title
 
-      ctx.response.body = ticket;
-      return;
-    case 'statusTicket':
-      const ticketStat = tickets.findIndex(item => +id === item.id);
-      tickets[ticketStat].status = status;
-    
-      ctx.response.body = true;
-      return;
-    case 'changeTicket':
-      const ticketIndex = tickets.findIndex(item => +id === item.id);
-  
-      tickets[ticketIndex].name = name;
-      tickets[ticketIndex].description = description;
-  
-      ctx.response.body = tickets[ticketIndex];
-      return;
-    case 'delTicketById':
-      const ticketDel = tickets.findIndex(item => item.id === +ID);
-      tickets.splice(ticketDel, 1);
-  
-      ctx.response.body = true;
-      return;
-    default:
-      ctx.response.status = 404;
-      return;
-    }
-});
+			ticketForEditFull.description = requstBody.description
+			ticketForEditFull.title = requstBody.title
 
-const port = process.env.PORT || 7070;
-const server = http.createServer(app.callback()).listen(port);
+			ctx.response.body = JSON.stringify(idEditIndex)
+			return
+
+		case "checkTicket":
+			const idcheckIndex = tickets.findIndex(({ id }) => ctx.request.query.id === id)
+
+			tickets[idcheckIndex].status = ctx.request.body.status
+			ticketsFull[idcheckIndex].status = ctx.request.body.status
+
+			ctx.response.body = JSON.stringify(idcheckIndex)
+			return
+		default:
+			ctx.response.status = 200
+			return
+	}
+})
+
+const server = http.createServer(app.callback())
+
+const port = 7070
+
+server.listen(port, err => {
+	if (err) {
+		console.log(err)
+
+		return
+	}
+
+	console.log("Server is listening to " + port)
+})
